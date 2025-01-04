1fcc60cc3fbf4ff8a801ef0ba0cb0a7f940752ac
Javier Stumpf <stumpfjavier@gmail.com>
2025-01-04 15:16:52 -0300

    Creación de tablas

---
+++ proyecto_rrhh/App/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+
+class Candidato(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    fecha_nacimiento = models.DateField()
+    ciudad = models.CharField(max_length=100)
+    provincia = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=50, choices=[
+        ('Programación', 'Programación'),
+        ('Data', 'Data'),
+        ('Redes', 'Redes'),
+        ('QA', 'QA'),
+        ('UX/UI', 'UX/UI'),
+        ])
+    seniority= models.CharField(max_length=50, choices=[
+        ('Junior', 'Junior'),
+        ('Semi Senior', 'Semi Senior'),
+        ('Senior', 'Senior'),
+        ('Manager', 'Manager'),
+        ])
+    archivo_cv = models.FileField(upload_to='cvs/')
+
+class Busqueda(models.Model):
+    puesto = models.CharField(max_length=100)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    categoria = models.CharField(max_length=50, choices=[
+        ('Programación', 'Programación'),
+        ('Data', 'Data'),
+        ('Redes', 'Redes'),
+        ('QA', 'QA'),
+        ('UX/UI', 'UX/UI'),
+        ])
+    seniority= models.CharField(max_length=50, choices=[
+        ('Junior', 'Junior'),
+        ('Semi Senior', 'Semi Senior'),
+        ('Senior', 'Senior'),
+        ('Manager', 'Manager'),
+        ])
+    tareas= models.TextField()
+    requisitos_excluyentes = models.TextField()
+    requisitos_deseables = models.TextField()
+    estado = models.CharField(max_length=50, choices=[
+        ('Activa', 'Activa'),
+        ('Finalizada', 'Finalizada')
+        ])
+
+class Postulacion(models.Model):
+    id_busqueda = models.ForeignKey(Busqueda, on_delete=models.CASCADE, related_name="postulaciones")
+    id_candidato = models.ForeignKey(Candidato, on_delete=models.CASCADE, related_name="postulaciones")
+    fecha_postulacion = models.DateField(auto_now_add=True)
+    estado = models.CharField(max_length=50, choices=[
+        ('Pendiente', 'Pendiente'),
+        ('Prueba técnica', 'Prueba técnica'),
+        ('Entrevistas', 'Entrevistas'),
+        ('Preocupacional', 'Preocupacional'),
+        ('Contratado', 'Contratado'),
+        ('Rechazado', 'Rechazado')
+        ])
